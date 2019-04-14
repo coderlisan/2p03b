c3bf57989684303ce5adb22164d9e4740abaaea3
coderlisan <coderlisan@gmail.com>
2019-04-14 08:43:03 +0600

    psd file just removed from bemax themeplate

--- js/custome.js
+++ js/custome.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
 
-$('.owl-carousel').owlCarousel({
+$('.slider-active').owlCarousel({
     items:1,
     loop:true,
     nav:true,
@@ -18,7 +18,43 @@ $('.owl-carousel').owlCarousel({
     }
 });
 
+
+// $('.porject-active').owlCarousel({
+//     items:1,
+//     loop:true,
+//     nav:true,
+//     navText:['<i class="fa fa-arrow-left fa-1x"></i>','<i class="fa fa-arrow-right fa-1x"></i>'],
+//     merge:true,
+//     responsive:{
+//         678:{
+//             mergeFit:true
+//         },
+//         1000:{
+//             mergeFit:false
+//         }
+//     }
+// });
+
+
+$('.porject-active').owlCarousel({
+    responsiveClass:true,
+    loop:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:4,
+        }
+    }
 })
+
+
+})
+
 
 
    // $('.slider-active').owlCarousel({
